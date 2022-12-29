09eb17355305778b72075189b187ac0a9e39ee15
bernas04 <joaobernardo0@ua.pt>
2022-12-29 19:18:48 +0000

    sda

--- frequent_count.py
+++ frequent_count.py
@@ -1,12 +1,24 @@
 import os
 from collections import defaultdict
 import unicodedata
+import random
+import string
+
+
+def letter_count(text):
+    counters = defaultdict(int)
+    for char in text:
+        if char.isalpha() and char != "":
+            char = remove_accents(char)
+            counters[char] += 1
+    return counters
 
 
 def frequent_count(text, threshold):
+    """function used to implement the frequent count algorithm"""
     counters = defaultdict(int)
     for char in text:
-        if char.isalpha():
+        if char.isalpha() and char != "":
             char = remove_accents(char)
             counters[char] += 1
 
@@ -21,15 +33,28 @@ def frequent_count(text, threshold):
     return frequent_items
 
 
-def csuros_counter(text, n):
-    counters = defaultdict(int)
-    for letter in text:
-        if letter.isalpha():
-            letter = remove_accents(letter)
-            counters[letter] += 1
+def incrementLetter(x, m):
+    t = x // m
+    while t > 0:
+        if random.getrandbits(1) == 1:
+            return x
+        t -= 1
+    return x + 1
 
-    sorted_counters = sorted(counters.items(), key=lambda x: x[1], reverse=True)
-    return sorted_counters[:n]
+
+def estimate_frequent_letters(text, threshold):
+    """function used to implement the Csuros counter algorithm"""
+    letter_count = {letter: 0 for letter in string.ascii_uppercase}
+
+    for char in text:
+        if char.isalpha():
+            char = remove_accents(char)
+            if char != "":
+                letter_count[char] = incrementLetter(letter_count[char], threshold)
+
+    sorted_letters = sorted(letter_count, key=lambda x: letter_count[x], reverse=True)
+
+    return sorted_letters, letter_count
 
 
 # remove accents from a string
@@ -45,7 +70,18 @@ if __name__ == "__main__":
         text = file.read().upper()
         file.close()
 
-        print("%10s - %10s" % ("Frequen Count", file.name.replace("collections/", "")))
+        print(file.name.replace("collections/", "").upper().center(24, "-"))
+        print("{:s}".format("Basic Counter"))
+        counter = letter_count(text)
+        for letter, freq in counter.items():
+            print("%5s : %3d" % (letter, freq))
+
+        print("{:s}".format("Csuros Counter"))
+        sorted_letters = estimate_frequent_letters(text, 5000)
+        letters, dict_freq = sorted_letters
+        for letter, freq in dict_freq.items():
+            print("%5s : %3d" % (letter, freq))
+        print(letters)
+
+        print("{:s}".format("Frequent Counter"))
         print(frequent_count(text, 5000), end="\n\n")
-        print("%10s" % ("Csuros Counter"))
-        print(csuros_counter(text, 3), end="\n\n")
